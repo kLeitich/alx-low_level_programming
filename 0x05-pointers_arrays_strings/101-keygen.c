dcd007d1ea347bd33f2d197183d3d7fff17be9d5
Kevin Leitich <kevleitich@gmail.com>
2021-06-28 10:22:22 +0300

    100 to 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,17 +1,33 @@
-#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
-*
-*
-*/
+ *main - generates random passwords
+ *
+ *Return:0(success)
+ *
+ */
 int main(void)
 {
+	int i, sum, n;
+	int pass[100];
 
-srand(time(0));
-int num = rand();
-printf("%i\n",num);
-return (0);
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
 }
-
