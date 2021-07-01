b63b9b76be01e092566df9a07eee009210aeb791
Kevin Leitich <kevleitich@gmail.com>
2021-07-01 21:48:40 +0300

    0 to 104

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,101 +1,104 @@
 #include "holberton.h"
+
 /**
- *rev_string - reverses a string
- *
- *@n:string input
- *
- *Return:void
- *
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
  */
-void rev_string(char *n)
+int _strlen(char *s)
 {
-	int i = 0, j = 0;
+	char *p = s;
+
+	while (*s)
+		s++;
+	return (s - p);
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: string s
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int size = _strlen(s);
 	char temp;
 
-	while (*(n + i) != '\0')
+	while (i < size)
 	{
+		temp = *(s + i);
+		*(s + i) = *(s + size - 1);
+		*(s + size - 1) = temp;
 		i++;
-	}
-	i--;
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
+		size--;
 	}
 }
+
 /**
- *infinite_add - adds two long numbers,stores them.in a buffer
- *
- *@n1:one of the two numbers
- *@n2:2nd no.
- *@r:buffer to store result
- *@size_r:buffer size
- *
- *Return:pointer to result
- *
+ * returnRes - changes pretotal to digit to be added
+ * @sum: pre-total
+ * @plusOne: flag to add one to res
+ * Return: returns digit to be placed into array
+ */
+int returnRes(int sum, int plusOne)
+{
+	int res;
+
+	if (sum == 9 && plusOne)
+		res = 0;
+	else if ((sum >= 10 && plusOne) || (sum < 9 && plusOne))
+		res = (sum % 10) + 1;
+	else
+		res = sum % 10;
+	return (res);
+}
+
+/**
+ * returnPlusOne - determines bool of plusOne
+ * @sum: pre-total
+ * @plusOne: flag to add one to res
+ * Return: 1 if true, 0 if false
+ */
+int returnPlusOne(int sum, int plusOne)
+{
+	if (sum > 9)
+		plusOne = 1;
+	else if (sum == 9 && plusOne)
+		plusOne = 1;
+	else
+		plusOne = 0;
+	return (plusOne);
+}
+
+/**
+ * infinite_add - function that adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer that the function will use to store the result
+ * @size_r: size of buffer
+ * Return: pointer to result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0;
-	int i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+	int sum, res, first, second, i = 0, plusOne = 0;
+	int len1 = _strlen(n1), len2 = _strlen(n2);
+	char *ptr = r;
 
-	while (*(n1 + i) != '\0')
+	while (len1 > 0 || len2 > 0)
 	{
+		first = len1 > 0 ? (*(n1 + len1 - 1) - '0') : 0;
+		second = len2 > 0 ? (*(n2 + len2 - 1) - '0') : 0;
+		sum = first + second;
+		res = returnRes(sum, plusOne);
+		plusOne = returnPlusOne(sum, plusOne);
+		*(ptr + i) = res + '0';
+		len1--;
+		len2--;
 		i++;
 	}
-	while (*(n2 + j) != '\0')
-	{
-		j++;
-	}
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
-	{
-		return (0);
-	}
-	while (j >= 0 || i >= 0 || overflow == 1)
-	{
-		if (i < 0)
-		{
-			val1 = 0;
-		}
-		else
-		{
-			val1 = *(n1 + i) - '0';
-		}
-		if (j < 0)
-		{
-			val2 = 0;
-		}
-		else
-		{
-			val2 = *(n2 + j) - '0';
-		}
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-		{
-			overflow = 1;
-		}
-		else
-		{
-			overflow = 0;
-		}
-		if (digits >= (size_r - 1))
-		{
-			return (0);
-		}
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		j--;
-		i--;
-	}
-	if (digits == size_r)
-	{
-		return (0);
-	}
-	*(r + digits) = '\0';
-	rev_string(r);
-	return (r);
+	if (plusOne)
+		*(ptr + i) = 1 + '0';
+	ptr[++i] = '\0';
+	rev_string(ptr);
+	return ((size_r > _strlen(ptr)) ? ptr : 0);
 }
