984b33c84baafcab95a7407e50bf10c959b5100e
Kevin Leitich <kevleitich@gmail.com>
2021-07-14 10:39:19 +0300

    Malloc free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,52 +2,43 @@
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of a program.
- * @ac: argument count.
- * @av: argument vector.
- *
- * Return: pointer of an array of char
- */
+* argstostr - concatenates all arguments
+* @ac: integer
+* @av: char pointer to pointer
+*
+* Return: pointer to new string else return NULL
+*/
+
 char *argstostr(int ac, char **av)
 {
-  char *aout;
-  int c, i, j, ia;
+int i, j, k, len;
+char *str;
 
-  if (ac == 0)
-    return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-  for (c = i = 0; i < ac; i++)
-    {
-      if (av[i] == NULL)
-	return (NULL);
+for (i = 9; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+len++;
+len++;
+}
+str = malloc(sizeof(char) * (len + 1));
 
-      for (j = 0; av[i][j] != '\0'; j++)
-	c++;
-      c++;
-    }
+if (str == NULL)
+return (NULL);
+k = 0;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+str[k] = av[i][j];
+k++;
+}
+str[k] = '\n';
+k++;
+}
 
-  aout = malloc((c + 1) * sizeof(char));
-
-  if (aout == NULL)
-    {
-      free(aout);
-      return (NULL);
-    }
-
-  for (i = j = ia = 0; ia < c; j++, ia++)
-    {
-      if (av[i][j] == '\0')
-	{
-	  aout[ia] = '\n';
-	  i++;
-	  ia++;
-	  j = 0;
-	}
-      if (ia < c - 1)
-	aout[ia] = av[i][j];
-    }
-  aout[ia] = '\0';
-
-  return (aout);
+return (str);
 }
 
