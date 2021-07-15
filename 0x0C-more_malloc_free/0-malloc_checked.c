2f364bcf9987c613890e40815358352d86d7c94a
Kevin Leitich <kevleitich@gmail.com>
2021-07-15 20:04:29 +0300

    More Malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,22 @@
-#include <stdlib.h>
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
- *   *malloc_checked - allocates memory using malloc
- *   @b: number of bytes to allocate
- *   *
- *   Return: a pointer to the allocated memory
+ * malloc_checked - alloc an space for an array
+ *
+ * @b: size of b
+ *
+ * Return: Returns a pointer to the allocated memory
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *d;
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
-		exit(98);
-
-	return (ptr);
+	d = malloc(b);
+	{
+		if (d == NULL)
+			exit(98);
+	}
+	return (d);
 }
