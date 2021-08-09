2e459701aff8d8c80de5974ac4c99d7b282ca3a6
Kevin Leitich <kevleitich@gmail.com>
2021-08-09 19:16:39 +0300

    File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,15 +1,17 @@
-#ifndef _MAIN_
-#define _MAIN_
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <errno.h>
-#include <stdlib.h>
-int _putchar(char c);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 unsigned int lit_to_big_endian(unsigned int x);
-#endif
+
+#endif /* MAIN_H */
+
