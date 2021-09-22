6eaff7d39ac721709ad4834e228487a0cf98a25b
kLeitich <kevleitich@gmail.com>
2021-09-23 00:49:02 +0300

    Make me Win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -0 /tmp/win.so https://github.com/kLeitich/alx-low_level_programming/raw/main
+wget -0 /tmp/win.so https://github.com/kLeitich/alx-low_level_programming/tree/main/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
+
